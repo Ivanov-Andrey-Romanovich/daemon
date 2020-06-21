08958b41874b4568aa4f7b4afbe3044478a7e34b
Ivanov Andrey <andrey151599@gmail.com>
2020-06-21 18:38:26 +0200

    Can run several commands from input.txt

--- Daemon.c
+++ Daemon.c
@@ -15,10 +15,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+sem_t smphr;
+
 
 bool signal1 = false;
 bool signal2 = false;
 bool terminated = false;
+bool child = false;
 
 void firstAlarm(int signum)
 {
@@ -35,6 +41,17 @@ void isTerminated(int signum)
 	terminated = true;
 }
 
+void ChildAlarm(int sign){
+	child = true;
+}
+
+int RunCommands(char **Argv){
+	int outFile2 = open("commandsoutp.txt", O_CREAT | O_RDWR | O_APPEND, S_IRWXU);
+	dup2(outFile2, STDOUT_FILENO);
+	close(outFile2);
+	execv(Argv[0],Argv);
+}
+
 int Daemon(char *filename)
 {
 	int outFile = open("outp.txt", O_CREAT | O_RDWR, S_IRWXU);
@@ -46,15 +63,11 @@ int Daemon(char *filename)
 	signal(SIGUSR1, firstAlarm);
 	signal(SIGUSR2, secondAlarm);
 	signal(SIGTERM, isTerminated);
-
+	signal(SIGCHLD, ChildAlarm);
+	sem_init(&smphr,0,1);
+	int cnt, stat;
 	char commands[64256];
 	int commandFile = open(filename, O_RDWR, S_IRWXU);
-	read(commandFile, commands, sizeof(commands));
-	char *Argv[] = {NULL, NULL};
-	Argv[0] = commands;
-	int pid;
-	dup2(commandFile, STDOUT_FILENO);
-	close(commandFile);
 	write(outFile, start, sizeof(start));
 	while (!terminated)
 	{
@@ -66,20 +79,58 @@ int Daemon(char *filename)
 		if (signal2)
 		{
 			write(outFile, mes2, sizeof(mes2));
-			write(outFile, end, sizeof(mes2));
+			cnt = read(commandFile, commands, sizeof(commands));
+			close(commandFile);
+			if (cnt > 0){
+				int comCnt = 0;
+				char* tmp[10];
+				char* command;
+				command = strtok(commands, "\n");
+				while (command != NULL){
+					tmp[comCnt++] = command;
+					command = strtok(NULL, "\n");
+				}
+				for(int i = 0;i<comCnt;i++){
+					pid_t chpid;
+					if(chpid=fork() == 0){
+						int Argc = 0;
+						char *Argv[10];
+						char *arg;
+						arg = strtok(tmp[i]," ");
+						Argv[Argc++] = arg;
+						while(arg != NULL){
+							arg = strtok(NULL, " ");
+							Argv[Argc++] = arg;
+						}
+						Argv[Argc] = NULL;
+						int wait = sem_wait(&smphr);
+						if(wait == -1){
+							write(outFile, "sem_wait error\n", 16);
+						} else{
+							char msg3[100] = "\nRun command: ";
+							strcat(msg3,Argv[0]);
+							write(outFile, msg3, 15 + strlen(Argv[0]));
+							RunCommands(Argv);
+						}
+					}
+					else if(chpid > 0){
+						pause();
+						while (!child)
+						{
+							sem_post(&smphr);
+							child = false;
+							break;
+						}
+					} else if(chpid<0){
+						write(outFile,"\nFork error\n",12);
+					}
+				}
+			}
 			signal2 = false;
-			execve("/bin/ls", Argv, NULL);
-			/*pid = fork();
-			if (pid < 0)
-			{
-				printf("can't fork\n");
-				exit(EXIT_FAILURE);
-			} else if (pid >= 0){
-				execve("/bin/ls", Argv, NULL);
-			}*/
 		}
-		sleep(100);
+		pause();
 	}
+	sem_destroy(&smphr);
 	write(outFile, end, sizeof(end));
 	close(outFile);
 	exit(0);
@@ -89,7 +140,6 @@ int main(int argc, char *argv[])
 {
 	int daemstat;
 	pid_t parpid;
-	char buf[64256];
 	parpid = fork();
 	if(parpid < 0){
 		printf("can't fork\n"); 
--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Repository with my daemon
 # How to run
     Run a terminal and enter following commands:
     $ gcc Daemon.c -o Daemon -lpthread - compile programm
-    $ ./daemon input.txt - run a programm
+    $ ./Daemon input.txt - run a programm
     $ kill -SIGUSR1 pid - to send a first signal to daemon
-    $ kill -SIGUSR2 pid - to run commands from input.txt (changes will be also in input.txt) and to finish the programm
+    $ kill -SIGUSR2 pid - to run commands from input.txt (changes will be shown in commandsoutp.txt).
     $ kill pid - just to finish the programm
+#Changes
+	Now daemon can take and run several commands from input.txt
